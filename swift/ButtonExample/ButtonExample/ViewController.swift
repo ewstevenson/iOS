72348163f9678233b56b449528489fff7f4892ab
ewstevenson <ewsutllc@gmail.com>
2016-06-05 08:25:36 -0600

    added button, outlet, and log print.

--- swift/ButtonExample/ButtonExample/ViewController.swift
+++ swift/ButtonExample/ButtonExample/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // CTRL + Click the button on the view controller, drag over. view storyboard and this file 
+    // in columns.
+    
+    // Button outlet
+    @IBOutlet weak var btnButton: UIButton!
+    
+    // ctrl + drag button to create new outlet
+    @IBOutlet weak var btnButton2: UIButton!
+    
+    @IBAction func buttonClicked(sender: UIButton) {
+        // print to the debug console when the button was clicked
+        NSLog("The button has been clicked");
+        
+    }
+    
+    
+    @IBAction func Button2Clicked(sender: UIButton) {
+        // print to the debug console when the button2 was clicked
+        NSLog("The button2 has been clicked");
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
